30854a32c13ea6c988368db9beb17f79c9a08c30
ItsArtem <mironartem923@gmail.com>
2025-04-25 08:54:43 +0000

    test

--- DocumentOborotMASProject/spravka/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import HttpResponse
-from django.shortcuts import render
-# Create your views here.
-def glavnaya(request):
-    return HttpResponse('А робит или нет?')
-def neglavnaya(request):
-    return render(request, 'index.html')
